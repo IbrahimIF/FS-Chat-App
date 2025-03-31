8874cd8e54065c0289ebb3cf2987dc38ee84b8cb
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2025-03-31 19:27:32 +0100

    Updated for deployment
    - Updated .gitignore to ignore the .env files created
    - Updated package.json start up to not use nodemon
    - updated App.js to use the current deployed backend URL

--- Simple-Room-Chat/Client/src/App.js
+++ Simple-Room-Chat/Client/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import io from 'socket.io-client'
 import { useEffect, useState }  from "react";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect( process.env.REACT_APP_API_URL || "http://localhost:3001");
 
 function App() {
   const [room, setRoom] = useState("");
