329768ece3fc261b755e4f932cf6529f48f2006f
IbrahimIF <ibrahimfarrah30@gmail.com>
2026-02-08 21:35:01 +0000

    updated Display.jsx, realtime listener and replacing fetch logic, updated index.js

--- S.E.R.N/Client/src/Components/Display/Display.jsx
+++ S.E.R.N/Client/src/Components/Display/Display.jsx
@@ -1,68 +1,26 @@
-import { useState, useEffect } from 'react';
-import { API_BASE_URL, API_ENDPOINTS } from '../../config';
+import { useEffect } from 'react';
 import './Display.css';
 
-function Display({ isSent, setIsSent }) {
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-  const [errorMessage, setErrorMessage] = useState('');
-
-  const fetchData = () => {
-
-    fetch(`${API_BASE_URL}${API_ENDPOINTS.MESSAGES}`, {
-    })
-      .then(response => response.json())
-      .then(fetchedData => {
-        if (fetchedData.length === 0) {
-          setErrorMessage('No data found.');
-          setData([]);
-        } else {
-          setData(fetchedData);
-          setErrorMessage('');
-        }
-        setIsLoading(false);
-      })
-      .catch(error => {
-        setErrorMessage('Error fetching data');
-        setIsLoading(false);
-        console.error('Fetch error:', error);
-      });
-  };
-
+function Display({ socket, room, messages, setMessages }) {
   useEffect(() => {
-    if (isSent) {
-      fetchData(); // Re-fetch data when the data is sent
-      setTimeout(() => setIsSent(false), 3000); // Hide the confirmation message after 3 seconds
-    }
-  }, [isSent, setIsSent]);
+    socket.on("receive_message", (data) => {
+      setMessages((prev) => [...prev, data.message]);
+    });
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    return () => socket.off("receive_message");
+  }, [socket, setMessages]);
 
-  if (isLoading) {
-    return (
-      <div className="displayContainer">
-        <div className="loadingContainer">
-          <div>Loading...</div>
-        </div>
-      </div>
-    );
+  if (!room) {
+    return <div className="displayContainer">Join a room to start chatting</div>;
   }
 
   return (
     <div className="displayContainer">
-      <div className="displayArea">
-        {errorMessage ? (
-          <div className="error">{errorMessage}</div>
-        ) : (
-          <ul className="text">
-            {data.map(item => (
-              <li key={item._id}>{item.name}: {item.text}</li>
-            ))}
-          </ul>
-        )}
-      </div>
+      <ul className="text">
+        {messages.map((msg, i) => (
+          <li key={i}>{msg}</li>
+        ))}
+      </ul>
     </div>
   );
 }
